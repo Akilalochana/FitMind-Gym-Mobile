b017a3a58a9eaff50e7bbfb5c1ba94e1b65a4f78
Akila Lochana <akilalochana7@gmail.com>
2025-10-17 12:53:42 +0530

    Add bottom app bar feature with home page

--- lib/main.dart
+++ lib/main.dart
@@ -1,29 +1,57 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_movie_app/pages/add_new_page.dart';
+import 'package:flutter_movie_app/pages/favourite_page%20.dart';
 import 'package:flutter_movie_app/pages/home_page.dart';
-
-
-
-
+import 'package:flutter_movie_app/pages/profile_page.dart';
 
 void main() {
   runApp(const MyApp());
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
   const MyApp({super.key});
 
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  int _currentIndex = 0;
+
+  final List<Widget> _pages = [
+    HomePage(),
+    AddNewPage(),
+    FavouritePage(),
+    ProfilePage(),
+  ];
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'FitMind Gym',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+      home: Scaffold(
+        bottomNavigationBar: BottomNavigationBar(
+          selectedItemColor: Colors.blueAccent,
+          unselectedItemColor: Colors.blueGrey,
+          currentIndex: _currentIndex,
+          onTap: (index) {
+            setState(() {
+              _currentIndex = index;
+            });
+          },
+          items: [
+            BottomNavigationBarItem(icon: Icon(Icons.home), label: 'Home'),
+            BottomNavigationBarItem(icon: Icon(Icons.add), label: 'Add New'),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.favorite),
+              label: 'Favourite',
+            ),
+            BottomNavigationBarItem(icon: Icon(Icons.person), label: 'Profile'),
+          ],
+        ),
+        body: _pages[_currentIndex],
       ),
-      home: const HomePage(),
-
-      
     );
   }
 }
--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -10,6 +10,10 @@ class HomePage extends StatefulWidget {
 class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      body: SafeArea(
+        child: SingleChildScrollView(child: Center(child: Text('Home Page'))),
+      ),
+    );
   }
 }
