a39b272e62bfc1f86c40e568a4b3a56db745d7bc
Akila Lochana <akilalochana7@gmail.com>
2025-10-21 05:25:35 +0530

    add models and assests

---
+++ lib/models/equipment_model.dart
@@ -0,0 +1,19 @@
+class Equipment {
+  final int id;
+  final String equipmentName;
+  final String equipmentDescription;
+  final String equipmentImageUrl;
+  final String noOfMinutes;
+  final String n0OfCalories;
+  final bool handOverd;
+
+  Equipment({
+    required this.id,
+    required this.equipmentName,
+    required this.equipmentDescription,
+    required this.equipmentImageUrl,
+    required this.noOfMinutes,
+    required this.n0OfCalories,
+    required this.handOverd,
+  });
+}
